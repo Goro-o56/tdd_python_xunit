9dbe211c397444325fb061351b21ba1cbf9710d0
goro-o56 <yamashitagoro2727@gmail.com>
2022-09-02 16:06:02 +0900

    first commit

---
+++ xunit.py
@@ -0,0 +1,15 @@
+
+class WasRun: #呼び出されたことを知るクラス
+  def __init__(self, name):
+    self.wasRun = None
+    self.name = name
+  def run(self):
+    method = getattr(self, self.name)
+    method()
+  def testMethod(self):
+    self.wasRun = 1
+
+test = WasRun("testMethod")
+print(test.wasRun)
+test.run()
+print(test.wasRun)
